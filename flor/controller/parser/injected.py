b07c013eddd79eb5ed08d6dc7cc5f8f805a423f9
Karina Uppal <kuppal@berkeley.edu>
2019-02-12 16:16:19 -0800

    Experiment Name :: pytorch_demo_nn

--- flor/controller/parser/injected.py
+++ flor/controller/parser/injected.py
@@ -31,7 +31,7 @@ def internal_log(v, d):
     d['__stack_frame__'] = tuple(stack_frame)
     file.write(json.dumps(d, indent=4) + ',\n')
     #if dict_of_returns:
-    import ipdb; ipdb.set_trace()
+    
     if (d['typ'] == 'metric'):
         writer.add_scalar('metric', v)
     return v
