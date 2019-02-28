41f991ed386a83078e786d872f775eac55d6430c
Karina Uppal <kuppal@berkeley.edu>
2019-02-28 10:19:49 -0800

    Experiment Name :: iris

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -18,11 +18,12 @@ def fit_and_score_model(gamma, C, test_size, random_state):
                                                   random_state=log.param(random_state))
 
     clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
-    clf.fit(X_tr, y_tr)
 
-    dummy_input = torch.rand(1, 3, 784)
+    dummy_input = torch.rand(1, 3)
     with SummaryWriter(comment='Support Vector') as w:
         w.add_graph(clf, dummy_input, True)
+
+    clf.fit(X_tr, y_tr)
 
     score = log.metric(clf.score(X_te, y_te))
 
