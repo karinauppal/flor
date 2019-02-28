ca67a0f369e76c37fb8da7627b6008d08033bce9
Karina Uppal <kuppal@berkeley.edu>
2019-02-28 10:12:11 -0800

    Experiment Name :: iris

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -3,6 +3,8 @@ from sklearn import datasets
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 
+from tensorboardX import SummaryWriter
+
 log = flor.log
 
 
@@ -16,6 +18,10 @@ def fit_and_score_model(gamma, C, test_size, random_state):
 
     clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
     clf.fit(X_tr, y_tr)
+
+    dummy_input = torch.rand(1, 3, 784)
+    with SummaryWriter(comment='Support Vector') as w:
+        w.add_graph(clf, dummy_input, True)
 
     score = log.metric(clf.score(X_te, y_te))
 
