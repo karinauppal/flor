fa280bb81c4e94a59fbce325397d3a4da532ec10
Karina Uppal <kuppal@berkeley.edu>
2019-03-04 10:49:59 -0800

    Experiment Name :: pytorch_demo_nn

--- examples/pytorch/pytorch_demo_nn.py
+++ examples/pytorch/pytorch_demo_nn.py
@@ -90,8 +90,8 @@ def main():
                 log.param(i)
                 log.metric(loss.item())
 
-                writer.add_scalar('loss_epoch', loss.item(), epoch)
-                writer.add_scalar('loss_iter', loss.item(), i)
+                writer.add_scalar('loss_epoch', loss.data.item(), epoch)
+                writer.add_scalar('loss_iter', loss.data.item(), i)
 
                 #writer.add_scalar('loss.item__', loss.item(), i)
 
