f902dc374881ad73704a7296baa6172a7931efb6
Karina Uppal <kuppal@berkeley.edu>
2019-03-10 21:11:27 -0700

    Experiment Name :: pytorch_demo_nn

--- examples/pytorch/pytorch_demo_nn.py
+++ examples/pytorch/pytorch_demo_nn.py
@@ -68,7 +68,7 @@ def main():
     # Train the model
     total_step = len(train_loader)
     for epoch in range(num_epochs):
-        #print(epoch)
+        print(epoch)
         for i, (images, labels) in enumerate(train_loader):
             # Move tensors to the configured device
             images = images.reshape(-1, 28*28).to(device)
