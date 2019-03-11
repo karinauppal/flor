45c8670a23168883d92814616ee8a6be9b370840
Karina Uppal <kuppal@berkeley.edu>
2019-03-11 16:33:11 -0700

    Experiment Name :: iris

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -37,6 +37,29 @@ log = flor.log
 #         fit_and_score_model(gamma=gamma, C=100.0, test_size=0.15, random_state=100)
 
 
+# @flor.track
+# def main():
+#     C = 100.0
+#     test_size = 0.15
+#     random_state = 100
+#     iris = datasets.load_iris()
+#     X_tr, X_te, y_tr, y_te = train_test_split(iris.data, iris.target,
+#                                                   test_size=log.param(test_size),
+#                                                   random_state=log.param(random_state))
+#     gammas = [0.001, 0.01, 0.05, 0.1]
+#     for gamma in gammas:
+#         clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
+#
+#         clf.fit(X_tr, y_tr)
+#
+#         score = log.metric(clf.score(X_te, y_te))
+#         writer.add_scalar('score', score, gamma)
+#         print('Gamma: ' , gamma)
+#         print('Score: ' , score)
+#
+# with flor.Context('iris'):
+#     main()
+
 @flor.track
 def main():
     C = 100.0
@@ -46,14 +69,13 @@ def main():
     X_tr, X_te, y_tr, y_te = train_test_split(iris.data, iris.target,
                                                   test_size=log.param(test_size),
                                                   random_state=log.param(random_state))
-    gamma = 0.001
-    for i in range(10):
+    for gamma in range(0.01, 0.1, 0.08):
         clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
 
         clf.fit(X_tr, y_tr)
 
         score = log.metric(clf.score(X_te, y_te))
-        writer.add_scalar('score', score, i)
+        writer.add_scalar('score', score, gamma)
         print('Gamma: ' , gamma)
         print('Score: ' , score)
 
