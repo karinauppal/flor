97d23aa3fd84bf94c4d4f5605f00f131a1f70f0c
Karina Uppal <kuppal@berkeley.edu>
2019-03-10 21:04:10 -0700

    Experiment Name :: pytorch_demo_nn

--- examples/pytorch/pytorch_demo_nn.py
+++ examples/pytorch/pytorch_demo_nn.py
@@ -87,9 +87,8 @@ def main():
             log.param(epoch)
             log.param(i)
             log.metric(loss.item())
-            
-            dummy_s1 = torch.rand(1)
-            writer.add_scalar('loss_epoch', dummy_s1[0], epoch)
+
+        writer.add_scalar('loss_epoch', loss.item(), epoch)
             # writer.add_scalar('loss_iter', loss.item(), i)
 
 
