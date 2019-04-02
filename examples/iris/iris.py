4d566d57b3c83e8e149de7de5cb3bf4c0abbd355
Karina Uppal <kuppal@berkeley.edu>
2019-04-01 23:14:53 -0700

    Experiment Name :: iris

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -49,4 +49,8 @@ with flor.Context('iris'):
 
             dct[str(gamma)].append(float(fit_and_score_model(gamma=gamma, C=100.0, test_size=0.15, random_state=100, iter=i)))
 
+        dct2 = {}
+        for key in dct:
+            dct2[key] = np.array(dct[key])
+
         writer.add_scalars('score', dct, i)
