b6664256dc21d7b0e5bc39d0339c00b7059984a2
Karina Uppal <kuppal@berkeley.edu>
2019-04-12 10:28:02 -0700

    used Sacred for iris.py

--- examples/iris/iris.py
+++ examples/iris/iris.py
@@ -1,56 +1,91 @@
-import flor
+# import flor
+# from sklearn import datasets
+# from sklearn import svm
+# from sklearn.model_selection import train_test_split
+#
+# import torch
+# import numpy as np
+#
+# from tensorboardX import SummaryWriter
+# from collections import defaultdict
+# writer = SummaryWriter()
+#
+# log = flor.log
+#
+# @flor.track
+# def fit_and_score_model(gamma, C, test_size, random_state, iter):
+#
+#     iris = datasets.load_iris()
+#     X_tr, X_te, y_tr, y_te = train_test_split(iris.data, iris.target,
+#                                                   test_size=log.param(test_size),
+#                                                   random_state=log.param(random_state))
+#
+#     clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
+#
+#     # want graph to look like one node for all libraries besides torch
+#     #input = torch.zeros(1, 3)
+#     #writer.add_graph(clf, input, True)
+#
+#     clf.fit(X_tr, y_tr)
+#
+#     score = log.metric(clf.score(X_te, y_te))
+#
+#     print('Gamma: ' , gamma)
+#     print('Score: ' , score)
+#
+#     return score
+#
+# gammas = [0.01, 0.05, 0.1]
+# with flor.Context('iris'):
+#     dct = defaultdict(list)
+#     for i in range(5):
+#         for gamma in gammas:
+#
+#             dct[str(gamma)] = float(fit_and_score_model(gamma=gamma, C=100.0, test_size=0.15, random_state=100, iter=i))
+#
+#         writer.add_scalars('score', dct, i)
+
 from sklearn import datasets
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 
 import torch
 import numpy as np
+
 from tensorboardX import SummaryWriter
 from collections import defaultdict
+writer = SummaryWriter()
 
-writer = SummaryWriter(filename_suffix='writer1')
-writer = SummaryWriter(filename_suffix='writer1')
-log = flor.log
+from sacred import Experiment
+ex = Experiment()
 
-
-@flor.track
-def fit_and_score_model(gamma, C, test_size, random_state, iter):
+@ex.capture
+def fit_and_score_model(gamma, C, test_size, random_state, iter, _run):
 
     iris = datasets.load_iris()
     X_tr, X_te, y_tr, y_te = train_test_split(iris.data, iris.target,
-                                                  test_size=log.param(test_size),
-                                                  random_state=log.param(random_state))
+                                                  test_size=test_size,
+                                                  random_state=random_state)
 
-    clf = svm.SVC(gamma=log.param(gamma), C=log.param(C))
-
-    # want graph to look like one node for all libraries besides torch
-    #input = torch.zeros(1, 3)
-    #writer.add_graph(clf, input, True)
+    clf = svm.SVC(gamma=gamma, C=C)
 
     clf.fit(X_tr, y_tr)
 
-    score = log.metric(clf.score(X_te, y_te))
-    # writer.add_scalar('score'_{0}'.format(gamma)', score, iter)
-    #writer.add_scalar('score', score, iter)
+    score = clf.score(X_te, y_te)
+    _run.log_scalar('score', score, iter)
 
     print('Gamma: ' , gamma)
     print('Score: ' , score)
 
     return score
 
-
-gammas = [0.01, 0.1]
-with flor.Context('iris'):
+@ex.automain
+def main():
+    gammas = [0.01, 0.05, 0.1]
     dct = defaultdict(list)
     for i in range(5):
         for gamma in gammas:
-            # if str(gamma) not in dict:
-            #     dct[str(gamma)] = np.empty((5,))
 
             dct[str(gamma)] = float(fit_and_score_model(gamma=gamma, C=100.0, test_size=0.15, random_state=100, iter=i))
-
-        # dct2 = {}
-        # for key in dct:
-        #     dct2[key] = np.array(dct[key])
 
         writer.add_scalars('score', dct, i)
